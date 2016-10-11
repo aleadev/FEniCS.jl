6e74730daab212cee0298ce8efa74fb4d374da83
Elmar Zander <e.zander@tu-bs.de>
2016-10-11 13:15:59 +0200

    [wip] See whether travis works now (with fenics installation and such...)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,11 @@ using FEniCS
 using Base.Test
 
 # write your own tests here
-@test 1 == 2
+# @test 1 == 2
+
+@testset "Mesh Tests" begin
+  mesh = UnitSquareMesh(2, 2)
+  coords = coordinates(mesh)
+  coordsex = [(j==0?mod(i,3):div(i,3))*0.5 for i=0:8, j=0:1]
+  @test coords == coordsex
+end
