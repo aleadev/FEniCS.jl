7ce5ab0b2f8b85eabedf1b58335a9718362ab1ed
Elmar Zander <e.zander@tu-bs.de>
2016-10-18 14:46:36 +0200

    Added mesh functions and tests

--- src/mesh.jl
+++ src/mesh.jl
@@ -4,20 +4,35 @@ hmin(mesh::Mesh) = fenicscall(mesh, :hmin)
 hmax(mesh::Mesh) = fenicscall(mesh, :hmax)
 rmin(mesh::Mesh) = fenicscall(mesh, :rmin)
 rmax(mesh::Mesh) = fenicscall(mesh, :rmax)
+cells(mesh::Mesh) = fenicscall(mesh, :cells)
 coordinates(mesh::Mesh) = fenicscall(mesh, :coordinates)
+init(mesh::Mesh) = fenicscall(mesh, :init)
+init(mesh::Mesh, dim::Int) = fenicscall(mesh, :init, dim)
 size(mesh::Mesh, dim::Int) = fenicscall(mesh, :size, dim)
+size(mesh::Mesh) = [size(mesh, dim) for dim = 0:3]
+num_cells(mesh::Mesh) = fenicscall(mesh, :num_cells)
+num_edges(mesh::Mesh) = fenicscall(mesh, :num_edges)
+num_entities(mesh::Mesh, dim::Int) = fenicscall(mesh, :num_entities, dim)
+num_faces(mesh::Mesh) = fenicscall(mesh, :num_faces)
+num_facets(mesh::Mesh) = fenicscall(mesh, :num_facets)
+num_vertices(mesh::Mesh) = fenicscall(mesh, :num_vertices)
 # TODO: add more mesh functions
+# closest_point(point::Point)::Point, closest_cell(point)::Int
 
 export
-  Mesh, hmin, hmax, rmin, rmax, coordinates
+  Mesh, hmin, hmax, rmin, rmax, init, coordinates, cells,
+  num_cells, num_edges, num_entities, num_faces, num_facets, num_vertices
+
+
 
 ## Mesh constructors
 UnitTriangleMesh() = Mesh(dolfin.UnitTriangleMesh())
-UnitSquareMesh(comm::MPICommunicator, nx::Int, ny::Int; diagonal::String="right") = Mesh(dolfin.UnitSquareMesh(comm, nx, ny, diagonal))
+#UnitSquareMesh(comm::MPICommunicator, nx::Int, ny::Int; diagonal::String="right") = Mesh(dolfin.UnitSquareMesh(comm, nx, ny, diagonal))
 UnitSquareMesh(nx::Int, ny::Int; diagonal::String="right") = Mesh(dolfin.UnitSquareMesh(nx, ny, diagonal))
 # TODO: add more mesh constructors (Box, Rectangle)
 # TODO: add mesh loading functions
 
+# TODO: sometime later: add MeshEditor (MeshTopology, MeshGeometry,...), Point
 export
   UnitTriangleMesh,
   UnitSquareMesh
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,28 @@ using Base.Test
 
 @testset "Mesh Tests" begin
   mesh = UnitSquareMesh(2, 2)
-  coords = coordinates(mesh)
-  coordsex = [(j==0?mod(i,3):div(i,3))*0.5 for i=0:8, j=0:1]
-  @test coords == coordsex
+  coords = [(j==0?mod(i,3):div(i,3))*0.5 for i=0:8, j=0:1]
+  @test coordinates(mesh) == coords
+  @test hmin(mesh) ≈ sqrt(0.5)
+  @test hmax(mesh) ≈ sqrt(0.5)
+  @test rmin(mesh) ≈ 0.5*(1-sqrt(0.5))
+  @test rmax(mesh) ≈ 0.5*(1-sqrt(0.5))
+  @test size(mesh, 0) == 9 # 9 nodes
+  @test size(mesh, 1) == 2*2*3+2*2 # edges
+  @test size(mesh, 2) == 8 # triangles (cells)
+  @test size(mesh, 3) == 0 # no 3d elements here
+  @test size(mesh) == [9, 16, 8, 0]
+
+  mesh = UnitSquareMesh(1, 1)
+  init(mesh, 1)
+  @test size(mesh) == [4, 5, 2, 0]
+  @test cells(mesh) == [0 1 3; 0 2 3]
+
+  mesh = UnitTriangleMesh()
+  init(mesh)
+  @test size(mesh) == [3, 3, 1, 0]
+  @test cells(mesh) == [0 1 2]
+
+
+
 end
